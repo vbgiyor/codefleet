71347590617ebcd4f6bdaed3eeb171d963562622
Shekhar Bhosale <vbgiyor@gmail.com>
2025-05-07 12:52:53 +0530

    Fix flake8 linter error - Django CI - Iteration 1

--- python/django-framework/apps/djangocommerce/orders/serializers.py
+++ python/django-framework/apps/djangocommerce/orders/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Order
 
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'user_id', 'book_id', 'quantity', 'total_price', 'created_at']
+        fields = ['id', 'user_id', 'book_id', 'quantity', 'total_price',
+                  'created_at']
