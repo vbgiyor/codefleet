ce06987961bbe0c5f1962f537a0eeadd8570b267
Shekhar Bhosale <vbgiyor@gmail.com>
2025-05-10 21:30:12 +0530

    Sign Up, Sign In, Sign Out,  Reset Password, Form Validations in Codefleet website

--- website/backend/codefleet/settings.py
+++ website/backend/codefleet/settings.py
@@ -93,10 +93,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    # BASE_DIR.parent / 'frontend/build/static',  # Correct path
-]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
