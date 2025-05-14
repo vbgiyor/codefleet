ea1623b9a2185ca4b1837cca237885adc55498a7
Shekhar <vbgiyor@gmail.com>
2025-05-14 14:38:39 +0530

    Add start-django.sh to encapsulate Django startup commands
    Add start-django.sh to encapsulate Django startup commands
    Fix website_and_cfinspector_ci.yml by using start-django.sh as entrypoint

---
+++ website/backend/start-django.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install -r /app/website/backend/requirements.txt
+cd /app/website/backend
+python3 manage.py migrate
+exec python3 manage.py runserver 0.0.0.0:8000
